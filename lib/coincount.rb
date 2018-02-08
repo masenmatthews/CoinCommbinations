2c5900f7e6b1e5527674759e6e3fdf179d85209c
Hannah Lee and Masen Matthews <masen@>
2018-02-08 10:56:15 -0800

    add passing spec for nickels

--- lib/coincount.rb
+++ lib/coincount.rb
@@ -6,10 +6,12 @@ class Change
   def calc()
     quarters = 0
     dimes = 0
-    quarters = (@money / 0.25).round()
+    quarters = (@money / 0.25).floor()
     remainder = (@money % 0.25).round(2)
-    dimes = (remainder / 0.10).round()
+    dimes = (remainder / 0.10).floor()
+    remainder = (remainder % 0.10)
+    nickels = (remainder % 0.05).ceil()
 
-  return "#{quarters} quarters and #{dimes} dimes"
+  return "#{quarters} quarters and #{dimes} dimes and #{nickels} nickels"
   end
 end
--- spec/coincount_spec.rb
+++ spec/coincount_spec.rb
@@ -5,11 +5,16 @@ require('coincount')
 describe('#coincount') do
   # it("takes 25 cents and returns 1 quarter") do
   #     change_return = Change.new(0.25)
-  #   expect(change_return.calc()).to(eq(1))
+  #   expect(change_return.calc()).to(eq("1 quarters and 0 dimes"))
+  # end
+  #
+  # it("takes 35 cents and returns 1 quarter and 1 dime") do
+  #   change_return = Change.new(0.35)
+  #   expect(change_return.calc()).to(eq("1 quarters and 1 dimes"))
   # end
 
-  it("takes 35 cents and returns 1 quarter and 1 dime") do
-    change_return = Change.new(0.35)
-    expect(change_return.calc()).to(eq("1 quarters and 1 dimes"))
+  it("takes 40 cents and returns 1 quarter, 1 dime, and 1 nickels") do
+    change_return = Change.new(0.40)
+    expect(change_return.calc()).to(eq("1 quarters and 1 dimes and 1 nickels"))
   end
 end
